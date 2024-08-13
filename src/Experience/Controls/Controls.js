c9940b39d1cc00fd3bd64982799c02e5ae0a39b1
MohammadROmar <149619529+MohammadROmar@users.noreply.github.com>
2024-08-13 11:35:59 +0300

    controls

--- src/Experience/Controls/Controls.js
+++ src/Experience/Controls/Controls.js
@@ -1,9 +1,10 @@
-import * as THREE from "three";
 import gsap from "gsap";
 
 import Experience from "../Experience.js";
 
 import MobileControls from "./MobileControls.js";
+
+import { constants } from "../Physics/constants.js";
 
 export default class Controls {
   constructor(model) {
@@ -21,7 +22,6 @@ export default class Controls {
     this.mobileControls = new MobileControls();
     this.mobileControlButtons = this.mobileControls.buttons;
 
-    this.addButtonsEventListeners();
     this.addKeyboardEventListeners();
   }
 
@@ -35,128 +35,66 @@ export default class Controls {
   }
 
   moveForwardEvent() {
-    gsap.to(this.modelPosition, {
-      duration: 2,
-      z: this.modelPosition.z - 3,
-    });
+    // this.modelPosition.z -= 0.01;
+    // this.cameraPosition.z -= 0.01;
+    // this.orbitControls.target.z -= 0.01;
 
-    gsap.to(this.cameraPosition, {
-      duration: 2,
-      z: this.cameraPosition.z - 3,
-    });
-
-    gsap.to(this.orbitControls.target, {
-      duration: 2,
-      z: this.modelPosition.z - 3,
-    });
+    constants.Yangle += 0.01;
   }
 
   moveRightwardEvent() {
-    gsap.to(this.modelRotation, {
-      duration: 2,
-      y: this.modelRotation.y - Math.PI / 10,
-    });
-
-    gsap.to(this.cameraRotation, {
-      duration: 2,
-      y: this.cameraRotation.y - Math.PI / 10,
-    });
-
-    // gsap.to(this.orbitControls.target, {
-    //   duration: 2,
-    //   z: this.modelPosition.z - 3
-    // });
+    constants.angle += 0.01;
   }
 
   moveLeftwardEvent() {
-    gsap.to(this.modelRotation, {
-      duration: 2,
-      y: this.modelRotation.y + Math.PI / 10,
-    });
-
-    gsap.to(this.cameraPosition, {
-      duration: 2,
-      y: this.modelRotation.y + Math.PI / 10,
-    });
+    constants.angle -= 0.01;
   }
 
   moveBackwardEvent() {
-    gsap.to(this.modelPosition, {
-      duration: 2,
-      z: this.modelPosition.z + 3,
-    });
-
-    gsap.to(this.cameraPosition, {
-      duration: 2,
-      z: this.cameraPosition.z + 3,
-    });
-
-    gsap.to(this.orbitControls.target, {
-      duration: 2,
-      z: this.modelPosition.z + 3,
-    });
+    // this.cameraPosition.z += 0.01;
+    constants.Yangle = 0.01;
   }
 
-  dive() {
-    gsap.to(this.modelPosition, {
-      duration: 2,
-      z: this.modelPosition.y - 1,
-    });
-  }
+  dive() {}
 
-  arise() {
-    gsap.to(this.modelPosition, {
-      duration: 2,
-      z: this.modelPosition.y + 1,
-    });
-  }
-
-  handlePress(event) {
-    // this.resetCameraPosition();
-
-    const key = event.target.getAttribute("data-key");
-
-    if (key === "w" || event.key === "w" || event.key === "W") {
-      this.moveForwardEvent();
-    } else if (key === "a" || event.key === "a" || event.key === "A") {
-      this.moveLeftwardEvent();
-    } else if (key === "s" || event.key === "s" || event.key === "S") {
-      this.moveBackwardEvent();
-    } else if (key === "d" || event.key === "d" || event.key === "D") {
-      this.moveRightwardEvent();
-    } else if (key === "e" || event.key === "e" || event.key === "E") {
-      this.arise();
-    } else if (key === "f" || event.key === "f" || event.key === "F") {
-      this.dive();
-    }
-  }
-
-  handleRelease(event) {
-    // const key = event.target.getAttribute("data-key");
-    // console.log(key);
-  }
-
-  addButtonsEventListeners() {
-    for (const key in this.mobileControlButtons) {
-      const button = this.mobileControlButtons[key];
-
-      /* Touch Event */
-      button.addEventListener("touchstart", (event) => this.handlePress(event));
-
-      button.addEventListener("touchend", (event) => this.handleRelease(event));
-
-      /* Mouse Clicking Event */
-      button.addEventListener("mousedown", (event) => this.handlePress(event));
-
-      button.addEventListener("touchend", (event) => this.handleRelease(event));
-    }
-  }
+  arise() {}
 
   addKeyboardEventListeners() {
-    /* Pressing & Holding the key */
-    document.addEventListener("keydown", (event) => this.handlePress(event));
+    const pressedKeys = {};
 
-    /* Releasing the key */
-    document.addEventListener("keyup", (event) => this.handleRelease(event));
+    /* Event to listen to key presses */
+    document.addEventListener("keydown", (event) => {
+      pressedKeys[event.key] = true;
+
+      /* To handle each key */
+      if (pressedKeys["w"] || pressedKeys["W"]) {
+        this.moveForwardEvent();
+      }
+
+      if (pressedKeys["a"] || pressedKeys["A"]) {
+        this.moveLeftwardEvent();
+      }
+
+      if (pressedKeys["s"] || pressedKeys["S"]) {
+        this.moveBackwardEvent();
+      }
+
+      if (pressedKeys["d"] || pressedKeys["D"]) {
+        this.moveRightwardEvent();
+      }
+
+      if (pressedKeys["e"] || pressedKeys["E"]) {
+        this.arise();
+      }
+
+      if (pressedKeys["f"] || pressedKeys["F"]) {
+        this.dive();
+      }
+    });
+
+    /* Handle relese the button */
+    document.addEventListener("keyup", (event) => {
+      pressedKeys[event.key] = false;
+    });
   }
 }
